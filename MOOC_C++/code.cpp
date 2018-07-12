83436f5912ca22b9aa7c33339843b2c3107ba92b
霖 汪 <becaged@outlook.com>
2018-07-12 18:05:26 +0800

    add class robot for translate numbers

--- MOOC_C++/code.cpp
+++ MOOC_C++/code.cpp
@@ -2,8 +2,6 @@
 #include "code.h"
 using namespace std;
 
-double Area(double a, double b, double c);
-
 double rate()
 {
 	double money, years, rate, sum;
@@ -73,19 +71,19 @@ int bubble_sort()
 }
 
 /* 计算三角形面积*/
-void triangle_Area()
-{
-	double a, b, c;
-	cout << "please input the triangle's three length of side to calculate its area.(separated by either commas or spaces)" << endl;;
-	cin >> a >> b >> c;
-	cout << "the area of this triangle is " << Area(a, b, c) << "." << endl;
-}
 double Area(double a, double b, double c)
 {
 	double size,p;
 	p = (a + b + c) / 2;
 	size = sqrt(p*(p - a)*(p - b)*(p - c));
 	return size;
+}
+void triangle_Area()
+{
+	double a, b, c;
+	cout << "please input the triangle's three length of side to calculate its area.(separated by either commas or spaces)" << endl;;
+	cin >> a >> b >> c;
+	cout << "the area of this triangle is " << Area(a, b, c) << "." << endl;
 }
 
 /*使用引用变量交换传入值，调用后会改变传入值*/
@@ -98,5 +96,113 @@ void swap(int &x, int &y)
 	return ;
 }
 
+/*递归函数 计算n!*/
+unsigned long int factorial_n(int n)
+{
+	return  (n > 0) ? (n*factorial_n(n - 1)) : 1;//?:运算，如果 n>0则调用自身，如果n=0则递归结束
+}
+void factorial()
+{
+	int n;
+	cout << "Input n" << endl;
+	cin >> n;
+	cout << factorial_n(n)<< endl;
+}
 
-
+#pragma region class robot
+const static char *num1[] ={
+	"","one","two","three","four","five","six","seven","eight","nine","ten",
+	"eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"
+};
+const static char *num2[] ={
+	"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"
+};
+robot::robot()//构造函数
+{
+	strcpy_s(name, "robot");
+	strcpy_s(type, "V1.0");
+}
+robot::~robot(){};//析构函数
+void robot::translate(int n)
+{
+	if (n > 2999999999 || n < 0)
+	{
+		cout << "Input illegal" << endl;
+	}
+	else
+	{
+		out(n);
+		flag = 0;
+	}
+	cout << endl;
+}
+void robot::cinnum()
+{
+	int num;
+	cin >> num;
+	translate(num);
+}
+void robot::tran_int(int a)
+{
+	if (!a)
+	{
+		cout << "zero";
+	}
+	int units, tens, hundreds;
+	hundreds = a / 100;
+	tens = (a - hundreds * 100) / 10;
+	units = a % 10;
+	if (hundreds > 0)
+	{
+		cout << num1[hundreds] << " " << "hundred ";
+	}
+	if ((hundreds > 0 || flag) && (tens > 0 || units > 0))
+	{
+		cout << "and ";
+	}
+	if (tens > 1)
+	{
+		cout << num2[tens];
+		if (units > 0)
+			cout << "-" << num1[units] << " ";
+		else
+			cout << " ";
+	}
+	else
+	{
+		cout << num1[units + tens * 10] << " ";
+	}
+}
+void robot::out(int n)
+{
+	if (n > 1000000000)
+	{
+		tran_int(n / 1000000000);
+		cout << "billion ";
+		flag = 1;
+		out(n % 1000000000);
+	}
+	else
+	{
+		if (n > 1000000)
+		{
+			tran_int(n / 1000000);
+			cout << "million ";
+			flag = 1;
+			out(n % 1000000);
+		}
+		else
+		{
+			if (n > 1000)
+			{
+				tran_int(n / 1000);
+				cout << "thousand ";
+				flag = 1;
+				out(n % 1000);
+			}
+			else
+				tran_int(n);
+		}
+	}
+}
+#pragma endregion
--- MOOC_C++/code.h
+++ MOOC_C++/code.h
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 /*double rate() 
   double Area(double a, double b, double c) 
 */
 
 double rate(); //用于计算利息
+
 int bubble_sort();//冒泡排序
+
 void triangle_Area();//计算三角形面积
+
+void factorial();//计算n的阶乘
+
+class robot
+{
+	char name[20];//机器人姓名
+	char type[20];//机器人型号
+	bool flag = 0;
+public:
+	robot();//构造函数
+	~robot();//析构函数
+	void translate(int n);
+	void cinnum();
+private:
+	void tran_int(int a);
+	void out(int n);
+};
